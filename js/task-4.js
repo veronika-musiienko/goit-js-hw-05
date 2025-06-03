f33cd869528792c82079955bc8c1fd5edc7b279f
Veronika <mnika5437@gmail.com>
2025-06-03 12:52:55 +0300

    fixed task-4 using methods filter() and reduce()

--- js/task-4.js
+++ js/task-4.js
@@ -1,6 +1,7 @@
-const getTotalBalanceByGender = (users, gender) =>
-    users.reduce((total, user) =>
-        user.gender === gender ? total + user.balance : total, 0);
+const getTotalBalanceByGender = (users, gender) => users.filter(user => user.gender === gender).reduce((total, user) => total + user.balance, 0);
+        
+
+
 const clients = [
 	{
     name: "Moore Hensley",
